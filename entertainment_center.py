214e52c9cee717cb274d935764d4456a526ca14d
Mayer5 <myself.bobo@gmail.com>
2015-06-07 18:55:26 -0700

    Movie trailer website (first release)
    This is a movie trailer website.

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,18 +6,28 @@ import fresh_tomatoes
 
 
 def load_json_info(json_file):
+    """
+    :param json_file: string name of the json file
+    :return: the data in json format
+    """
     with open(json_file) as data_file:
         data = json.load(data_file)
     return data
 
 
 def generate_movie_lists(json_file="movies.json"):
+    """
+    :param json_file: string name of the json file which contains movies' info
+    :return: a list contain movies' information
+    """
     movies = []
     movies_info = load_json_info(json_file)
     for movie_value in movies_info.values():
-        movies.append(media.Movie(movie_value["title"], movie_value["image"], movie_value["url"]))
+        movies.append(media.Movie(movie_value["title"], movie_value["image"], movie_value["url"], movie_value["storyline"]))
     return movies
 
 if __name__ == '__main__':
     movies_list = generate_movie_lists()
+    # generate a list which contains movies info
     fresh_tomatoes.open_movies_page(movies_list)
+    # use fresh_tomatoes to generate the website
--- media.py
+++ media.py
@@ -2,8 +2,16 @@ __author__ = 'Mayer'
 
 
 class Movie(object):
-    def __init__(self, title, image, trailer_url):
+    def __init__(self, title, image, trailer_url, storyline):
+        """
+        :param title:
+        :param image:
+        :param trailer_url:
+        :param storyline:
+        :return: a Movie object which contains above info
+        """
         self.title = title
         self.poster_image_url = image
         self.trailer_youtube_url = trailer_url
+        self.storyline = storyline
 
